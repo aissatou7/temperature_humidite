03591668459f0c3ab0df7594fdc5303147951d6f
aissatou7 <yayefallsaliou@gmail.com>
2023-01-04 18:59:29 +0000

    test

---
+++ src/app/table-user/table-user.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table-user',
+  templateUrl: './table-user.component.html',
+  styleUrls: ['./table-user.component.scss']
+})
+export class TableUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

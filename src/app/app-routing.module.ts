9ff3f48f2614f201ac081629cd9aa3d431931689
aissatou7 <yayefallsaliou@gmail.com>
2023-01-04 18:16:34 +0000

    bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TableUserComponent } from './table-user/table-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: TableUserComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

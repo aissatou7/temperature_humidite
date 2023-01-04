3a229f6780788ab70afbd95395faab227313bf12
cheihsall <chmouhammed2605@gmail.com>
2023-01-04 20:17:03 +0100

    elioration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TableUserComponent } from './table-user/table-user.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DashbordComponent } from './dashbord/dashbord.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TableUserComponent
+    TableUserComponent,
+    SidebarComponent,
+    NavbarComponent,
+    DashbordComponent
   ],
   imports: [
     BrowserModule,

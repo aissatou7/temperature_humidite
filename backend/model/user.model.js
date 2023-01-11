20e3de97947b962029c1805ebf5c61f1b92aafc9
MHDLamine <mouhamed.ngom50@gmail.com>
2023-01-11 15:21:16 +0000

    search style

--- backend/model/user.model.js
+++ backend/model/user.model.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 var uniqueValidator = require('mongoose-unique-validator');
 const userSchema = mongoose.Schema({
-    firstName: {type: String, required: false},
-    lastName: {type: String, required: false},
+    prenom: {type: String, required: false},
+    nom: {type: String, required: false},
     email: {type: String, required: false, unique: true},
     matricule: {type: String, required: false},
     password: {type: String, required: false},
     role: {type: String, required: false},
-    etat: {type: Number, required: false}
+    etat: {type: Boolean, required: false}
 },
 {
     collection: 'books'

ee0f70de46d40b07b6b7c9b2f977ab017cb7dc89
kadidia <diallokhadidiatoubouba@gmail.com>
2023-01-11 12:12:06 +0000

    ok

--- backend/model/user.model.js
+++ backend/model/user.model.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 var uniqueValidator = require('mongoose-unique-validator');
 const userSchema = mongoose.Schema({
-    firstName: {type: String, required: false},
-    lastName: {type: String, required: false},
+    prenom: {type: String, required: false},
+    nom: {type: String, required: false},
     email: {type: String, required: false, unique: true},
     matricule: {type: String, required: false},
     password: {type: String, required: false},

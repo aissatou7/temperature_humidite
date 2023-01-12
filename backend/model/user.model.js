c4182d661233aca230fe1e939f357edcdce2b6db
kadidia <diallokhadidiatoubouba@gmail.com>
2023-01-12 15:59:24 +0000

    ok

--- backend/model/user.model.js
+++ backend/model/user.model.js
@@ -1,26 +1,30 @@
 
 const mongoose = require ('mongoose');
-
+/* var uniqueValidator = require('mongoose-unique-validator'); */
 const userSchema = mongoose.Schema(
 
 {
+
 prenom: { type:String},
 nom: { type:String},
 email: { type:String},
 matricule: { type:String},
 password: { type:String},
 role: { type:String},
-etat: { type:String},
+etat: { type:Boolean},
+
 
 },
+{timestamps: true},
 
-{
-collection:'utilisateur'
+/* {
+collection:'books'
 
-}
+} */ );
+/* userSchema.plugin(uniqueValidator , { message: "email exite deja"}); */
 
 
-);
 
-module.exports = mongoose.model('Model' , userSchema);
+module.exports = mongoose.model('User', userSchema);
+
 

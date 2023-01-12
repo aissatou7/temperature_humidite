2785c47698aa7e139c79cac8d1a8f79c6fd34e41
MHDLamine <mouhamed.ngom50@gmail.com>
2023-01-12 15:57:24 +0000

    box shadow

--- backend/model/user.model.js
+++ backend/model/user.model.js
@@ -1,9 +1,9 @@
-
 const mongoose = require ('mongoose');
-
+/* var uniqueValidator = require('mongoose-unique-validator'); */
 const userSchema = mongoose.Schema(
 
 {
+
 prenom: { type:String},
 nom: { type:String},
 email: { type:String},
@@ -12,15 +12,15 @@ password: { type:String},
 role: { type:String},
 etat: { type:Boolean},
 
+
 },
+{timestamps: true},
 
-{
+/* {
 collection:'books'
+} */ );
+/* userSchema.plugin(uniqueValidator , { message: "email exite deja"}); */
 
-}
 
 
-);
-
-module.exports = mongoose.model('User' , userSchema);
-
+module.exports = mongoose.model('User', userSchema);
